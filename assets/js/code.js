823eeea327b330d8ff724848ef57e3d3d52a404f
unodosmangos <morellicj@yahoo.com>
2022-12-18 14:29:47 -0500

    Avance 1.2

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    alert('Hola Mundo')
-})
+let productos = [
+    {
+        hamburguesa: 'SINGLE CHEESEBURGER',
+        precio: 13000,
+        imagen: "https://livingnomads.com/wp-content/uploads/2019/02/17/beetroot-burger-sydney.3.32.jpg",
+        descripcion:'Hamburguesa de una carne de 125 g cada una 100% de res a la parrilla con una tajada de queso tipo americano, tocineta, lechuga, tomate y cebolla, en pan brioche + papas medianas (francesas o cascos) + bebida PET'    
+    },
+    {
+        hamburguesa: 'DOUBLE CHEESEBURGER',
+        precio: 15000,
+        imagen: "https://www.kitchensanctuary.com/wp-content/uploads/2021/05/Double-Cheeseburger-square-FS-42.jpg",
+        descripcion:'Hamburguesa de dos carnes de 125 g cada una 100% de res a la parrilla con una tajada de queso tipo mozzarella, cebolla, tomate, lechuga  y salsa BigShot, en pan ajonjolí + papas medianas (francesas o cascos) + bebida PET'
+    },
+    {
+        hamburguesa: 'DOUBLE BACON CHEESEBURGER',
+        precio: 18000,
+        imagen: "https://www.sargento.com/assets/Uploads/Recipe/Image/burger_0.jpg",
+        descripcion:'Hamburguesa de dos carnes de 125 g cada una 100% de res a la parrilla con dos tajadas de queso tipo americano y dos tajadas de queso tipo mozzarella, cebella caramelizada y tocineta, en pan brioche + papas medianas (francesas o cascos) + bebida PET'
+    }
+]
