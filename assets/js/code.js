0dfd1b536bcb84a6de4865e9d3340197e692627c
unodosmangos <morellicj@yahoo.com>
2023-01-05 20:37:07 -0500

    avance - js menu

--- assets/js/code.js
+++ assets/js/code.js
@@ -12,9 +12,23 @@ let productos = [
         descripcion:'Hamburguesa de dos carnes de 125 g cada una 100% de res a la parrilla con una tajada de queso tipo mozzarella, cebolla, tomate, lechuga  y salsa BigShot, en pan ajonjolí + papas medianas (francesas o cascos) + bebida PET'
     },
     {
-        hamburguesa: 'DOUBLE BACON CHEESEBURGER',
+        hamburguesa: 'BACON DOUBLE CHEESEBURGER',
         precio: 18000,
         imagen: "https://www.sargento.com/assets/Uploads/Recipe/Image/burger_0.jpg",
         descripcion:'Hamburguesa de dos carnes de 125 g cada una 100% de res a la parrilla con dos tajadas de queso tipo americano y dos tajadas de queso tipo mozzarella, cebella caramelizada y tocineta, en pan brioche + papas medianas (francesas o cascos) + bebida PET'
     }
 ]
+
+let menuProductos = document.getElementById('menuBurgerShot')
+
+for (const menuHamburguesa of productos) {
+    menuProductos.innerHTML += `
+        <div class="d-flex flex-column align-items-center mx-4">
+            <a href="" class="text-decoration-none text-white fs-3">${menuHamburguesa.hamburguesa}</a>
+            <i type="button" class="fa-solid fa-chevron-up fs-1 text-danger"></i>
+            <a type="button" class="btn btn-danger col-4 styleProductos1" href="#" data-bs-toggle="modal" data-bs-target="#producto1"
+                 style="background-image: url(${menuHamburguesa.imagen});"></a>
+            <i type="button" class="fa-solid fa-chevron-down fs-1 text-danger"></i>
+        </div>
+    `
+}
